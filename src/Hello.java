69d0be7e816bcb977f3cd5e856cb6f3dbf42c0f7
Cpl-flowers <126724988+Cpl-flowers@users.noreply.github.com>
2025-01-29 22:32:11 +0000

    Hello world 1st save

--- src/Hello.java
+++ src/Hello.java
@@ -1,1 +1,10 @@
-// Hello World
+// this is a class or object which has to be named after the file it's in
+public class Hello {
+
+    // String[] args parameter for a string array
+    public static void main(String[] args) {
+        // print and println are similar to 
+        // cout << "hello": and cout << "hello" << endl;
+        System.out.println("Hello World!");
+    }
+}
